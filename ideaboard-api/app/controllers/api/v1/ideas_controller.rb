a80efa4c6011dc7ce35c05862149abc58283a77f
Your Actual Name <Your Actual Email>
2019-06-11 18:20:28 +0800

    changes

--- ideaboard-api/app/controllers/api/v1/ideas_controller.rb
+++ ideaboard-api/app/controllers/api/v1/ideas_controller.rb
@@ -1,37 +1,46 @@
 module Api::V1
   class IdeasController < ApplicationController
-  
+
     def index
-      @ideas = Idea.order("created_at ASC")
+      @ideas = Idea.order(created_at: :asc)
+
       render json: @ideas
     end
 
     def create
       @idea = Idea.create(idea_params)
-       ActionCable.server.broadcast 'ideas', event: :created, idea: @idea
+
+      ActionCable.server.broadcast(:ideas, event: :created, idea: @idea)
+
       render json: @idea
     end
 
     def update
       @idea = Idea.find(params[:id])
       @idea.update_attributes(idea_params)
-       ActionCable.server.broadcast 'ideas', event: :updated, idea: @idea
+
+      ActionCable.server.broadcast(:ideas, event: :updated, idea: @idea)
+
       render json: @idea
     end
 
     def destroy
       @idea = Idea.find(params[:id])
+
       if @idea.destroy
-        head :no_content, status: :ok
+        ActionCable.server.broadcast(:ideas, event: :deleted, idea: @idea)
+
+        head :no_content
       else
         render json: @idea.errors, status: :unprocessable_entity
       end
     end
-    
-	private	
-	def idea_params
-		params.require(:idea).permit(:title, :body, :color)
-		end
-	end
-end 
+
+    private
+
+    def idea_params
+    	params.require(:idea).permit(:title, :body, :color)
+    end
+  end
+end
 
