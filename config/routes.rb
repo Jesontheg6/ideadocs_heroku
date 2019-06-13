4fcaac43682d7a6e9ab0cbed6edbb5d680bbde3d
Your Actual Name <Your Actual Email>
2019-06-13 08:21:36 +0800

    moved package.json

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :ideas
     	resources :boards
+
+    	get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+  !request.xhr? && request.format.html?
+end
     end
   end
 end
