e33cf1522a65dbaee03151fe174c5b6c8d39a44e
Your Actual Name <Your Actual Email>
2019-06-13 11:07:19 +0800

    changes

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'active_job/railtie'
 require 'active_record/railtie'
 require 'active_storage/engine'
 require 'action_controller/railtie'
-require 'action_mailer/railtie'
+# require 'action_mailer/railtie'
 require 'action_view/railtie'
 require 'action_cable/engine'
 require 'rails/test_unit/railtie'
@@ -28,6 +28,6 @@ module IdeaboardApi
       end
     end
 
-    # config.action_cable.mount_path = '/cable'
+    config.action_cable.mount_path = '/cable'
   end
 end
